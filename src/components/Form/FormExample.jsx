c0e2b35b42517fc485963fcdb3a7fe645386949c
pruthvisinhvaghela <pruthvivaghela19@gmail.com>
2024-05-21 11:54:54 +0530

    learn how to use multi input in one function with different name attribute

--- src/components/Form/FormExample.jsx
+++ src/components/Form/FormExample.jsx
@@ -5,15 +5,36 @@ import { useState } from "react";
 
 const FormExample = () => {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleChange = (e) => {
+    // console.log(e.target.value);
+    if (e.target.name === "firstName") {
+      const capName = e.target.value.toUpperCase();
+      setName(capName);
+    } else {
+      setPassword(e.target.value);
+    }
     console.log(e.target.value);
-    setName(e.target.value);
   };
   return (
     <div>
       <form>
-        <input type="text" value={name} onChange={handleChange} />
+        <label htmlFor="">firstname</label>
+        <input
+          type="text"
+          name="firstName"
+          value={name}
+          onChange={handleChange}
+        />
+        <br />
+        <label htmlFor="">password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
